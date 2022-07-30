0ad8a5025d06b74a1b83405f6c2a1264d6bac6ae
송기훈 <gihun3645@naver.com>
2022-07-30 20:01:09 +0900

    jsp 설정이 안됨..

--- board/src/main/java/com/study/board/test/TempControllerTest.java
+++ board/src/main/java/com/study/board/test/TempControllerTest.java
@@ -1,12 +1,31 @@
 package com.study.board.test;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+// Controller 는 해당 경로 밑에 있는 파일을 리턴
 public class TempControllerTest {
 
+    @GetMapping("/home")
     public String tempHome() {
+        System.out.println("tempHome()");
+        // 파일리턴 기본경로 : src/main/resources/static
+        // 리턴명 : /home/html
+        // 풀경로 : src/main/resources/static/home.html
+        return "/home.html";
+    }
 
-        return "home.html";
+    @GetMapping("/img")
+    public String tempImg() {
+        return "/a.png";
+    }
+
+    @GetMapping("/jsp") // jsp는 정적인 파일이 아닌 동적인 파일
+    public String tempJsp() {
+        // prefix : /WEB-INF/views/
+        // suffix : .jsp
+        // 풀네임 : /WEB-INF/views/test.jsp
+        return "test";
     }
 }
