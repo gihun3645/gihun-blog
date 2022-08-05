468b04ba6d7c0011d74a93f6ea7c2033cc371798
송기훈 <gihun3645@naver.com>
2022-08-05 17:39:09 +0900

    id로 select 테스트

--- board/src/main/java/com/study/board/test/DummyControllerTest.java
+++ board/src/main/java/com/study/board/test/DummyControllerTest.java
@@ -4,9 +4,14 @@ import com.study.board.model.RoleType;
 import com.study.board.model.User;
 import com.study.board.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.function.Supplier;
+
+// html파일이 아니라 data를 리턴해주는 controller = RestController
 @RestController
 public class DummyControllerTest {
 
@@ -15,6 +20,38 @@ public class DummyControllerTest {
 
     //localhost:8080/dummy/join (요청)
     // http의 body에 username, password, email 데이터를 가지고 (요청)
+
+    // {id} 주소로 파라미터를 전달 받을 수 ㅇㅆ음.
+    // localhost:3000/dummy/join/user/3
+    @GetMapping("/dummy/user/{id}")
+    public User detail(@PathVariable int id) {
+
+        // user/4을 찾으면 내가 데이터베이스에서 못찾아오게 되면 user가 null이 될 것 아냐?
+        // 그럼 return null 이 리턴됨 그럼 프로그램에 문제가 있지 않갰니?
+        // Optional 로 너의 User 객체를 감싸서 가져올테니 null 인지 아닌지 판단해서 return 해!
+
+//        // 람다식
+//        User user = userRepository.findById(id).orElseThrow(()->{
+//            return new IllegalArgumentException("해당 사용자는 없습니다.");
+//        });
+
+        User user = userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>() {
+            @Override
+            public IllegalArgumentException get() {
+                // TODO Auto-generated method stub
+                return new IllegalArgumentException("해당 유저는 없습니다. id : "+id);
+            }
+        });
+        // 요청 : 웹브라우자
+        // user 객체 = 자바 오브젝트
+        // 변환 (웹브라우저가 이해할 수 있는 데이터 -> json(Gson 라이브러리))
+        // 스프링부트 = MessageConvert라는 애가 응답시에 자동 작동
+        // 만약에 자바 오브젝트를 리턴하게 되면 MessageConvert가 Jackson 라이브러리를 호출해서
+        // user 오브젝트를 json으로 변환해서 브라우에게 던져준다.
+        
+        return user;
+    }
+
     @PostMapping("/dummy/join")
     public String join(User user) { // key=value (약속된 규칙)
         // 오브젝트로도 받을 수 있음
