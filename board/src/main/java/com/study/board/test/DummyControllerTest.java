2168a9f1c857c4ec9186b00511769c98c2ad328a
송기훈 <gihun3645@naver.com>
2022-08-06 17:37:24 +0900

    전체 select 및 paging 테스트

--- board/src/main/java/com/study/board/test/DummyControllerTest.java
+++ board/src/main/java/com/study/board/test/DummyControllerTest.java
@@ -4,11 +4,16 @@ import com.study.board.model.RoleType;
 import com.study.board.model.User;
 import com.study.board.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 // html파일이 아니라 data를 리턴해주는 controller = RestController
@@ -18,10 +23,25 @@ public class DummyControllerTest {
     @Autowired // 의존성 주입(DI)
     private UserRepository userRepository;
 
-    //localhost:8080/dummy/join (요청)
-    // http의 body에 username, password, email 데이터를 가지고 (요청)
+    // localhost:3000/dummy/user/
+    @GetMapping("/dummy/users")
+    public List<User> list() {
 
-    // {id} 주소로 파라미터를 전달 받을 수 ㅇㅆ음.
+        return userRepository.findAll();
+    }
+
+    // 한 페이지당 2건의 데이터를 리턴받아 볼 예정
+    @GetMapping("/dummy/user")
+    public List<User> pageList(@PageableDefault(size = 2, sort = "id",
+            direction = Sort.Direction.DESC)Pageable pageable) {
+
+        Page<User> pagingUser = userRepository.findAll(pageable);
+        
+        List<User> users = userRepository.findAll(pageable).getContent();
+        return users;
+    }
+
+    // {id} 주소로 파라미터를 전달 받을 수 있음.
     // localhost:3000/dummy/join/user/3
     @GetMapping("/dummy/user/{id}")
     public User detail(@PathVariable int id) {
@@ -48,7 +68,7 @@ public class DummyControllerTest {
         // 스프링부트 = MessageConvert라는 애가 응답시에 자동 작동
         // 만약에 자바 오브젝트를 리턴하게 되면 MessageConvert가 Jackson 라이브러리를 호출해서
         // user 오브젝트를 json으로 변환해서 브라우에게 던져준다.
-        
+
         return user;
     }
 
