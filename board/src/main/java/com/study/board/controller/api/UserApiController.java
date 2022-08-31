bd00bb9292703cddbde96de654aed7e7cadc7e44
송기훈 <gihun3645@naver.com>
2022-08-31 17:16:24 +0900

    비밀번호 해쉬 후 회원가입

--- board/src/main/java/com/study/board/controller/api/UserApiController.java
+++ board/src/main/java/com/study/board/controller/api/UserApiController.java
@@ -7,6 +7,7 @@ import com.study.board.model.User;
 import com.study.board.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,12 +20,9 @@ public class UserApiController {
     @Autowired
     private UserService userService;
 
-
     @PostMapping("/auth/joinProc")
     public ResDto<Integer> save(@RequestBody User user){ // username, password, email
         System.out.println("UserApiController: save 호출됨");
-        // 실제로 DB에 insert를 하고 아래에서 return이 되면 된다.
-        user.setRole(RoleType.USER);
         userService.회원가입(user);
         return new ResDto<Integer>(HttpStatus.OK.value(), 1); // 자바오브젝트를 JSON으로 변환해서 리턴(Jackson)
     }
--- board/src/main/java/com/study/board/service/UserService.java
+++ board/src/main/java/com/study/board/service/UserService.java
@@ -1,8 +1,10 @@
 package com.study.board.service;
 
+import com.study.board.model.RoleType;
 import com.study.board.model.User;
 import com.study.board.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,9 +16,16 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder encoder;
+
     // 서비스 함수
     @Transactional // 성공하면 커밋, 실패하면 롤백
     public void 회원가입(User user) {
+        String rawPassword = user.getPassword(); // 1234 원문
+        String encPassword = encoder.encode(rawPassword); // 해쉬화
+        user.setPassword(encPassword);
+        user.setRole(RoleType.USER);
         userRepository.save(user);
     }
 }
